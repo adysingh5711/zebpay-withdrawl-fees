df5363e3b04e432b13a62a78780772f4af7e59b0
Aditya <singhaditya5711@gmail.com>
2025-09-28 11:56:22 +0530

    Fix GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
 /** @type {import('next').NextConfig} */
 module.exports = {
     output: 'export',
-    basePath: (isProd && isGitHubPages) ? '/zebpay-withdraw-fees' : '',
-    assetPrefix: (isProd && isGitHubPages) ? '/zebpay-withdraw-fees/' : '',
+    basePath: (isProd && isGitHubPages) ? '/zebpay-withdrawl-fees' : '',
+    assetPrefix: (isProd && isGitHubPages) ? '/zebpay-withdrawl-fees/' : '',
     images: {
         unoptimized: true,
     },
